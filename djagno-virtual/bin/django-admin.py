0369c8c76f297e90ec2806d45ddc776866409155
Sunil-ghodela <ss.sunil9255@gmail.com>
2018-09-28 09:29:32 +0530

    starting django girl server

---
+++ djagno-virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/curofy/Desktop/name/project/py/django-1st-week/django-girl-implementation/djangoGirl-implement/djagno-virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
