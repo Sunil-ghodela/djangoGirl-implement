e7dad1b15034a7f4a1d94fbb11e5d6425c865a4a
Sunil-ghodela <ss.sunil9255@gmail.com>
2018-09-30 15:12:19 +0530

    django girl implementing

--- djangoGirl/blog/urls.py
+++ djangoGirl/blog/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.urls import path
 from blog import views
+from dashing.utils import router
 
 urlpatterns = [
-
+    url(r'dashboard/', include(router.urls)),
+    url(r'post_list/', views.post_list, name = 'post_list'),
 ]
--- djangoGirl/blog/views.py
+++ djangoGirl/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
 # Create your views here.
+
+
+def post_list(request):
+
+    post = Post.objects.filter(
+        publish_date__lte=timezone.now()).order_by('publish_date')
+
+    return render(request, 'post/post_list.html', {'posts': post})
